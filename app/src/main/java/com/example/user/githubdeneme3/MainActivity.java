b52f4a272e71937665ea0223a60ef512aa38f5c4
Hikmet Anıl <iyibidunya@gmail.com>
2018-11-13 16:18:26 +0200

    githubmet-newGithubmet

--- app/src/main/java/com/example/user/githubdeneme3/MainActivity.java
+++ app/src/main/java/com/example/user/githubdeneme3/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String deneme="githubmet";
+        String deneme="githubmet-newGithubmet";
     }
 }
